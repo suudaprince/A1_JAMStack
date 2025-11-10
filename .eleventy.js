4020cf85c8361fb58922cca5169aff990e095f5e
suudaprince <gprince1@confederationcollege.ca>
2025-11-09 23:20:34 -0500

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
